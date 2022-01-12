706bd12856a027a839928d8f9c0f521075c57dee
Denis Mishin <Denis_Mishin@epam.com>
2022-01-12 21:49:29 +0400

    Delete redundant imports.

--- src/main/java/com/example/chemistryapp/resource/AddServlet.java
+++ src/main/java/com/example/chemistryapp/resource/AddServlet.java
@@ -3,7 +3,6 @@ package com.example.chemistryapp.resource;
 import com.example.chemistryapp.service.ChemistryService;
 
 import javax.inject.Inject;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
--- src/main/java/com/example/chemistryapp/resource/MainServlet.java
+++ src/main/java/com/example/chemistryapp/resource/MainServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.Locale;
 
 
 @WebServlet(value = "/hello-servlet", urlPatterns = "/hello-servlet")
